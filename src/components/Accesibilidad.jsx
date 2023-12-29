13a9a84e22175b25b3c04d2d16a0901bba7fb33a
Renzo Barros <renzobarros89@gmail.com>
2023-12-29 08:38:34 -0300

    flotante sin antd

--- src/components/Accesibilidad.jsx
+++ src/components/Accesibilidad.jsx
@@ -1,23 +1,20 @@
 import React, { useState } from "react";
-import { FloatButton } from "antd";
-import { ControlOutlined } from "@ant-design/icons";
 import FloatComponent from "./FloatComponent";
+import styles from "./FloatComponent.module.css";
 
 const Accesibilidad = () => {
   const [showTable, setShowTable] = useState(false);
+
   return (
     <>
       {showTable && <FloatComponent setShowTable={setShowTable} />}
 
-      <FloatButton
-        shape="circle"
-        type="primary"
-        style={{
-          right: 94,
-        }}
-        icon={<ControlOutlined />}
+      <button
+        className={styles.floatButton}
         onClick={() => setShowTable(!showTable)}
-      />
+      >
+        Open
+      </button>
     </>
   );
 };
