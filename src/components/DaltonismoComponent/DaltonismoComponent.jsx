bed79e899933aaa5431f593e2518dad9e799f26e
Renzo Barros <renzobarros89@gmail.com>
2024-01-03 11:12:37 -0300

    daltonismo btn

---
+++ src/components/DaltonismoComponent/DaltonismoComponent.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const DaltonismoComponent = () => {
+  const [tiposDaltonismo, setTiposDaltonismo] = useState([
+    "",
+    "protanopia",
+    "deuteranopia",
+    "tritanopia",
+    "monocromo",
+  ]);
+  const [indiceTipoDaltonismo, setIndiceTipoDaltonismo] = useState(0);
+
+  const cambiarTipoDaltonismo = () => {
+    const nuevoIndice = (indiceTipoDaltonismo + 1) % tiposDaltonismo.length;
+    setIndiceTipoDaltonismo(nuevoIndice);
+    aplicarEstiloDaltonismo(tiposDaltonismo[nuevoIndice]);
+  };
+
+  const aplicarEstiloDaltonismo = (tipo) => {
+    const root = document.documentElement;
+    switch (tipo) {
+      case "protanopia":
+        root.style.cssText = `filter: hue-rotate(30deg) !important;`;
+        break;
+      case "deuteranopia":
+        root.style.cssText = `filter: hue-rotate(60deg) !important;`;
+        break;
+      case "tritanopia":
+        root.style.cssText = `filter: hue-rotate(120deg) !important;`;
+        break;
+      case "monocromo":
+        root.style.cssText = `filter: grayscale(100%) !important;`;
+        break;
+      // Agrega más casos según sea necesario para otros tipos de daltonismo
+      default:
+        // Restaurar el filtro predeterminado si no se selecciona un tipo de daltonismo
+        root.style.cssText = `filter: none;`;
+    }
+  };
+
+  return (
+    <div>
+      <h2>Configuración de Daltonismo</h2>
+      <button onClick={cambiarTipoDaltonismo}>
+        {tiposDaltonismo[indiceTipoDaltonismo] || "Ninguno"}
+      </button>
+
+      <p>Ejemplo de texto con filtro de hue-rotate ajustado para daltonismo.</p>
+    </div>
+  );
+};
+
+export default DaltonismoComponent;
