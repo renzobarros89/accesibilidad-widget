4f38086882fc27116e6dac84f0bdb58ee5dd26cf
Renzo Barros <renzobarros89@gmail.com>
2024-01-08 10:58:22 -0300

    se agrega shortcut alt+a

--- src/components/Accesibilidad/Accesibilidad.jsx
+++ src/components/Accesibilidad/Accesibilidad.jsx
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FloatComponent from "./FloatComponent";
 import styles from "./Accesibilidad.module.css";
 
 const Accesibilidad = () => {
   const [showTable, setShowTable] = useState(false);
 
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.altKey && event.key === "a") {
+        // Activa o desactiva showTable según su estado actual
+        setShowTable((prevShowTable) => !prevShowTable);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+
   return (
     <>
       <FloatComponent showTable={showTable} setShowTable={setShowTable} />
       <button
-        className={styles.floatButton}
+        className={`${styles.floatButton} ${showTable ? styles.showTable : ""}`}
         onClick={() => setShowTable(!showTable)}
       >
         <i className="fa-solid fa-universal-access"></i>
+        <span className={styles.tooltip}>Alt + A</span>
       </button>
     </>
   );
