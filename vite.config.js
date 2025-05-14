6d9c6eb04b590978cda50ca8f30e64d68e559691
Renzo Barros <renzobarros89@gmail.com>
2025-05-14 08:33:54 -0300

    Cambios previos al deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
+// vite.config.js
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/",
+  base: "/accesibilidad-widget/",
   plugins: [react()],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+    "process.env": {}, // previene otros errores con process.env.X
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/main-widget.jsx"),
